5cfbad72508f0f5aa11f817517841e6de28af6d5
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-12-06 15:52:19 -0500

    Changes keycloak angular version to avoid error (the user profile cannot be loaded)

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=emi
-export DOCKERHUB_TAG=0.1.19
+export DOCKERHUB_TAG=0.1.20
 
 rm -rf deployment/docker/dist/
 cp -R $FRONTEND_SHELL_PATH/dist deployment/docker/dist
